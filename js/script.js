ca210e6ae22f845f8035fb142df83dde9dc9ac60
shafiquddin <shafiquddin2k@gmail.com>
2026-07-25 00:53:46 +0530

    sticky

--- js/script.js
+++ js/script.js
@@ -37,6 +37,27 @@ allinks.forEach(function (link) {
   });
 });
 
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  },
+);
+
+obs.observe(sectionHeroEl);
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
